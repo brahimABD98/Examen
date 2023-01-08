c4ff416bb68911ddd66e3b4ac8f3e001f60eae3f
brahimABD98 <brahim.abderrahim@esprit.tn>
2023-01-08 20:58:00 +0100

    aop

--- src/main/java/tn/esprit/Examen/Aspect/LoggingAspect.java
+++ src/main/java/tn/esprit/Examen/Aspect/LoggingAspect.java
@@ -1,2 +1,18 @@
-package tn.esprit.Examen.Aspect;public class LoggingAspect {
+package tn.esprit.Examen.Aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+    @Before("execution(public * tn.esprit.Examen.Service.*.ajouter*(..))")
+    public void logMethodEntry(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info(" méthode exécutée " + name + " : ");
+    }
 }
